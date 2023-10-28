ec23d5f750def2a9be30b0bc4475640f2352bcf1
Dell <olegalcevski@gmail.com>
2023-10-28 11:13:03 +0200

    Submisson program1

---
+++ Homework1/Tree.py
@@ -0,0 +1,10 @@
+num_list = [4,5,67,89,23,23,134,15,4,23,29,212,94,98]
+max_num = max(num_list)
+print("Max:",max_num)
+min_num = min(num_list)
+print("Min:", min_num)
+sum_of_nums = sum(num_list)
+print("Sum:", sum_of_nums)
+length = len(num_list)
+avr_sum = sum_of_nums/length
+print("Average sum:", avr_sum)

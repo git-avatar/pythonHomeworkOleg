15ff2c47fe5d623ea78c95ffb0aa7c2df05b3ac0
Dell <olegalcevski@gmail.com>
2023-11-04 17:41:21 +0100

    Submisson program1

--- Homework1/Tree.py
+++ Homework1/Tree.py
@@ -1,10 +1,12 @@
-num_list = [4,5,67,89,23,23,134,15,4,23,29,212,94,98]
-max_num = max(num_list)
-print("Max:",max_num)
-min_num = min(num_list)
-print("Min:", min_num)
-sum_of_nums = sum(num_list)
-print("Sum:", sum_of_nums)
-length = len(num_list)
-avr_sum = sum_of_nums/length
-print("Average sum:", avr_sum)
+layers = int(input("Enter the number of layers"))
+tree_trunk_size = int(input("Size of the tree trunk"))
+for i in range(1, layers + 1):
+    spaces = " " * (layers - i)
+    stars = "*" * (2 * i - 1)
+    print(spaces + stars)
+
+
+for i in range(tree_trunk_size):
+    print("  " * (layers//2) + "|")
+
+
